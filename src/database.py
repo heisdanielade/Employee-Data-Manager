56ab58a517846490d87703eceee161591960d66b
heisdanielade <heisdanielade@gmail.com>
2024-04-23 00:18:11 +0200

    code refractoring

--- src/database.py
+++ src/database.py
@@ -1,156 +1,132 @@
-
-# -----------PROJECT DETAILS------------
 """
-    Adetunji Daniel Adediran - 114083 - UZ
-    A program that creates a database in a text file which stores user's
-      data with the ability to modify and sort data based on required arguments.
+    FILENAME: database.py
+    This file contains program for performing CRUD operations on the database.
 """
 
-
-# Handle possible module or import error
-try:
-    from datetime import datetime
-    from tabulate import tabulate
-    import random, json
-except (ModuleNotFoundError, ImportError) as e:
-    print(f"\n\n(e) ERROR: {e}\n\n")
+import json
+from datetime import datetime
+from tabulate import tabulate
+import random
 
 
-
-employee_data = []
-
+# File where employee data is stored
 filename = "db.txt"
 
 
-# all actions that can be performed by the user on the database
+# Class containing function(s) for possible user actions
 class UserActions:
-    print("\n----------------------WELCOME----------------------\n")
-    print("(i) Kindly select which action you would like to perform."
+    @staticmethod
+    def main():
+        print("\n----------------------WELCOME----------------------\n")
+        print("(i) Kindly select which action you would like to perform.\n"
               "\n- Type 'add' to add new employees"
-              "\n- Type 'all' to get list of all employee data"
+              "\n- Type 'all' to get a list of all employee data"
+              "\n- Type 'edit' to edit an existing employee's data"
               "\n- Type 'all-sort-age' to get list of employees sorted by age"
               "\n- Type 'all-sort-sal' to get list of employees sorted by salary"
               "\n- OR Type 'exit' to stop the code.")
-    
 
-    def main():
+
+        # check user input and match to designated function
         while True:
-            user_action = input("\n-> ACTION: ")
-            
-            # perform action based on user input
+            user_action = input("\n-> ACTION: ").lower()
             if user_action == 'add':
                 number_of_people = int(input("\nHow many employees to add: \n- "))
-                DatabaseActions.main(number_of_people, filename)
-            elif user_action == 'all':
-                DatabaseActions.get_data(employee_data)
-            elif user_action == 'all-sort-age':
-                DatabaseActions.sort_data_by_age(employee_data)
-            elif user_action == 'all-sort-sal':
-                DatabaseActions.sort_data_by_salary(employee_data)
+                DatabaseActions.add_employees(number_of_people)
+            elif user_action == 'edit':
+                employee_data = DatabaseActions.read_data()
+                DatabaseActions.edit_employee(employee_data)
+            elif user_action in ['all', 'all-sort', 'all-sort-age', 'all-sort-sal']:
+                employee_data = DatabaseActions.read_data()
+                if user_action == 'all-sort-age' or user_action == 'all-sort':
+                    employee_data = sorted(employee_data, key=lambda x: x['age'])
+                elif user_action == 'all-sort-sal':
+                    employee_data = sorted(employee_data, key=lambda x: x['salary'])
+                DatabaseActions.tabulate_data(employee_data)
             elif user_action == 'exit':
                 break
             else:
                 print("\n(i) Kindly enter a valid action.")
 
 
-# all actions that can be performed on database
+# Class containing function(s) for actions that can be performed on the database
 class DatabaseActions:
+    # function to store the employee data in the designated file
     @staticmethod
-    def store_data(data, database):
-        with open(filename, "w") as database:
-            # save data to file
-            json.dump(data, database)
-            print("\n\n(s) STATUS: Data saved successfully.\n\n")
-    
+    def store_data(data):
+        with open(filename, "w") as db:
+            json.dump(data, db)
+
+
+    # function to read the employee data from the text file
+    @staticmethod
+    def read_data():
+        try:
+            with open(filename, "r") as db:
+                return json.load(db)
+        except (FileNotFoundError, json.JSONDecodeError):
+            return []
+
+
+    # function to tabulate the employee data for a more presentable interface
     @staticmethod
     def tabulate_data(data):
-        try:
-            # Better formatted headers for the table
-            raw_header = [{'Created At':'', 'ID':'', 'First Name':'', 'Last Name':'', 'Age':'', 'Salary':''}]
-            header = raw_header[0].keys()
-            rows = [x.values() for x in data]
-            data_table = tabulate(rows, header)
-            print(data_table)
-        except ValueError as e:
-            print(f"\n(e) ERROR: {e}\n")
-        else:
-            print("\n\n(s) STATUS: Data received successfully.\n\n")
+        headers = ['Created At', 'ID', 'First Name', 'Last Name', 'Age', 'Salary']
+        table = [list(emp.values()) for emp in data]
+        print(tabulate(table, headers=headers, tablefmt='grid'))
 
+
+    # function to add new employee data to the text file
     @staticmethod
-    def get_data(employee_data):
-        DatabaseActions.tabulate_data(employee_data)
-    
-    def main(number_of_people, filename):
-        number_of_people = number_of_people
-
-        # function to handle invalid inputs from user
-        def get_input(prompt):
-            while True:
-                try:
-                    user_input = int(input(prompt))
-                    return user_input  # Return the input if it's successfully converted to an integer
-                except ValueError as e:
-                    print(f"\n\n(e) ERROR: {e}\n")
-                    print("(i) Kindly enter a valid input.\n")
-
-        for i in range(number_of_people):
-            # get user data
-            first_name = input("\n-> FIRST NAME {}: \n- ".format(i+1)) 
-            last_name = input("-> LAST NAME {}: \n- ".format(i+1))
-            age = get_input("-> AGE {}: \n- ".format(i+1))
-            salary = get_input("-> MONTHLY SALARY {}: \n- ".format(i+1))
-
-            # generate time properties in database
-            raw_time = datetime.now()
-            time_created = raw_time.strftime("%d/%b/%y %H:%M:%S")
-
+    def add_employees(number_of_people):
+        employee_data = DatabaseActions.read_data()
+        for _ in range(number_of_people):
+            first_name = input("\n-> FIRST NAME: \n- ") 
+            last_name = input("-> LAST NAME: \n- ")
+            age = int(input("-> AGE: \n- "))
+            salary = float(input("-> MONTHLY SALARY: \n- "))
+            time_created = datetime.now().strftime("%d/%b/%y %H:%M:%S")
             employee_id = random.randint(12468, 92680)
-            
-            # store data in dictionary then save to employee list
-            person = {
-                'created_at':time_created,
+            employee_data.append({
+                'created_at': time_created,
                 'id': employee_id,
                 'first_name': first_name,
                 'last_name': last_name,
                 'age': age,
-                'salary': salary  
-            }
-            employee_data.append(person)
-        
-
-        print("---Confirming data---")
-        print(f"\n{employee_data}\n")
-        print(f"\nFile path: {filename}\n")
-        DatabaseActions.store_data(employee_data, filename)
-
-        
-    def sort_data_by_age(employee_data):
-        print("\n[----Data sorted by AGE----]\n")
-        sorted_employee_list = sorted(employee_data, key = lambda x: x['age'])
-
-        # Print the sorted list
-        DatabaseActions.tabulate_data(sorted_employee_list)
+                'salary': salary
+            })
+        DatabaseActions.store_data(employee_data)
+        print("\n (s) STATUS: Employee data saved successfully.\n")
 
 
-    def sort_data_by_salary(employee_data):
-            print("\n[----Data sorted by SALARY----]\n")
-            sorted_employee_list = sorted(employee_data, key = lambda x: x['salary'])
-
-            # Print the sorted list
-            DatabaseActions.tabulate_data(sorted_employee_list)
+    # function to edit employee data by selecting the employee's ID
+    @staticmethod
+    def edit_employee(employee_data):
+        employee_id = int(input("\n-> Enter the ID of the employee you want to edit: "))
+        found = False
+        for emp in employee_data:
+            if emp['id'] == employee_id:
+                # if id is found then print the employee's current data
+                found = True
+                print("\n-> Current Data:")
+                DatabaseActions.tabulate_data([emp])
+                emp['first_name'] = input("-> Enter new first name (leave blank to keep current): ") or emp['first_name']
+                emp['last_name'] = input("-> Enter new last name (leave blank to keep current): ") or emp['last_name']
+                new_age = input("-> Enter new age (leave blank to keep current): ")
+                if new_age:
+                    emp['age'] = int(new_age)
+                new_salary = input("-> Enter new salary (leave blank to keep current): ")
+                if new_salary:
+                    emp['salary'] = float(new_salary)
+                break
+        if found:
+            DatabaseActions.store_data(employee_data)
+            print("\n (s) STATUS: Employee data updated successfully.\n")
+        else:
+            print("\n (s) STATUS: Employee data not found.\n")
 
 
 if __name__ == "__main__":
-    user = UserActions
-    # db = DatabaseActions
- 
-    user.main()
-    # db.main()
-    # db.get_data(employee_data)
-    # db.sort_data_by_age(employee_data)
-else:
-    print("(i) An error occured...")
-
-
+    UserActions.main()
 
 
